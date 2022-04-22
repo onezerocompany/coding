e3f17cc4b4eae7680e060039ed3fbd9914d27492
Luca Silverentand <luca@onezero.company>
2022-04-22 11:41:45 +0200

    :tada: feat/new(tsconfigs): add tsconfigs package
    Signed-off-by: Luca Silverentand <luca@onezero.company>

---
+++ packages/tsconfig/validate.spec.js
@@ -0,0 +1,32 @@
+/* eslint-disable strict */
+const { readdirSync, readFileSync } = require('fs');
+const { resolve } = require('path');
+const Ajv = require('ajv-draft-04');
+const fetch = require('node-fetch');
+
+const ajv = new Ajv({
+  strict: false,
+  allErrors: true,
+});
+
+let schema = null;
+
+describe('check if tsconfigs is valid', () => {
+  const baseFiles = readdirSync(resolve(__dirname, 'bases'));
+
+  beforeAll(async () => {
+    schema = await (await fetch('http://json.schemastore.org/tsconfig')).json();
+  });
+
+  for (const file of baseFiles) {
+    // eslint-disable-next-line no-loop-func
+    it(`${file} should be valid`, () => {
+      expect.assertions(1);
+      const filePath = resolve(__dirname, 'bases', file);
+      const fileContent = JSON.parse(readFileSync(filePath, 'utf8'));
+      const validate = ajv.compile(schema);
+      const valid = validate(fileContent);
+      expect(valid).toBe(true);
+    });
+  }
+});
