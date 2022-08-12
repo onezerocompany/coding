26c46a2c756bdf4fef0cec7554a72ae2da5eea89
Luca Silverentand <luca@onezero.company>
2022-08-12 19:39:16 +0200

    :star2: feat/impr(release-issues): update depended items
    Signed-off-by: Luca Silverentand <luca@onezero.company>

--- actions/release-issues/src/lib/items/update/updateReleaseClearance.ts
+++ actions/release-issues/src/lib/items/update/updateReleaseClearance.ts
@@ -1,35 +1,51 @@
+import type { VersionTrack } from '@onezerocompany/commit';
 import type { Globals } from '../../../globals';
 import type { Item } from '../Item';
 import { ItemStatus } from '../ItemStatus';
 import { wasItemChecked } from '../wasItemChecked';
 
+export async function dependenciesDone(
+  globals: Globals,
+  item: Item,
+  track: VersionTrack,
+): Promise<boolean> {
+  // only continue if the track is specified in the item metadata
+  const dependedItems = item.metadata.dependsOn
+    .map((dependsOn) => globals.context.issue.itemForType(dependsOn, track))
+    .filter((dependedItem: Item | null) => dependedItem !== null) as Item[];
+
+  // make sure the depended items are updated
+  const updates = dependedItems.map(async (dependedItem) =>
+    dependedItem.update(globals),
+  );
+  await Promise.all(updates);
+
+  // depending on other items, they should either be succeeded or skipped
+  const depenciesSucceeded = dependedItems.every(
+    (dependedItem) =>
+      dependedItem?.status === ItemStatus.succeeded ||
+      dependedItem?.status === ItemStatus.skipped,
+  );
+
+  return depenciesSucceeded;
+}
+
 export async function updateReleaseClearance(
   globals: Globals,
   item: Item,
 ): Promise<ItemStatus> {
+  // return status if status type is definitive result
+  if (item.status === ItemStatus.succeeded || item.status === ItemStatus.failed)
+    return item.status;
+
   const { track } = item.metadata;
   if (track) {
-    // only continue if the track is specified in the item metadata
-    const dependedItems = item.metadata.dependsOn.map((dependsOn) =>
-      globals.context.issue.itemForType(dependsOn, track),
-    );
-
-    // depending on other items, they should either be succeeded or skipped
-    const depenciesSucceeded = dependedItems.every(
-      (dependedItem) =>
-        dependedItem?.status === ItemStatus.succeeded ||
-        dependedItem?.status === ItemStatus.skipped,
-    );
-
-    if (depenciesSucceeded) {
+    if (await dependenciesDone(globals, item, track)) {
       const trackSettings = globals.settings[track];
       // in case the release is not manual, we don't need to do anything
       if (!trackSettings.release.manual) return ItemStatus.skipped;
 
-      if (
-        item.status === ItemStatus.succeeded ||
-        wasItemChecked(globals, item)
-      ) {
+      if (wasItemChecked(globals, item)) {
         // if the item was checked or was previously succeeded, we mark the item as succeeded
         return ItemStatus.succeeded;
       }
